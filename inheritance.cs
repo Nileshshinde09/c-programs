655f8fdadd280d8e851246d7450e3afdb04a384c
Nilesh Shinde <90444068+Nileshshinde09@users.noreply.github.com>
2022-12-15 15:55:16 +0530

    Add files via upload

---
+++ inheritance.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class A
+    {
+        private int num1;
+        public int num2;
+        public void Add(int a,int b)
+        {
+            num1 = a;
+            num2 = b;
+            Console.WriteLine("Addition of " + num1 + "and " + num2 + " is " + (a + 10));
+        }
+        public virtual void Add(int a)
+        {
+            Console.WriteLine("Addition is " + (a + 10));
+        }
+    }
+    class Program:A
+    {
+        public override void Add(int a)
+        {
+            Console.WriteLine("Addition is " + (a + 10));
+        }
+        static void Main(string[] args)
+        {
+            A obj = new A();
+            obj.num2 = 30;
+            obj.Add(10, obj.num2);
+            obj.Add(15);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ oop_reactangle_Area.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class rectangle
+    {
+        public int length, width;
+        public void input(int x,int y)
+        {
+            length = x;
+            width = y;
+        }
+        public int rectarea()
+        {
+            int area;
+            area = length * width;
+            return (area);
+        }
+    }
+    class area
+    {
+        static void Main(string[] args)
+        {
+            int area1, area2;
+            rectangle rect1 = new rectangle();
+            rectangle rect2= new rectangle();
+            rect1.length = 15;
+            rect1.width = 10;
+            area1 = rect1.length * rect1.width;
+            rect2.input(5,4);
+            area2 = rect2.rectarea();
+            Console.WriteLine("Area = " + area1);
+            Console.WriteLine("Area = " + area2);
+            Console.ReadLine();
+        }
+    }
+}
